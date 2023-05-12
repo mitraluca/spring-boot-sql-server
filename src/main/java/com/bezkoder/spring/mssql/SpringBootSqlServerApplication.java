5d380ea040c1f84d37f7000fe2f3e4b3276b240a
luca <mitra.miragliotta@gmail.com>
2023-05-12 13:46:41 +0200

    STARTING SERVICES WITH WITHOUT CONNECTION DB AND ADD SWAGGER-UI PAGE

--- src/main/java/com/bezkoder/spring/mssql/SpringBootSqlServerApplication.java
+++ src/main/java/com/bezkoder/spring/mssql/SpringBootSqlServerApplication.java
@@ -2,12 +2,16 @@ package com.bezkoder.spring.mssql;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableWebMvc
+@EnableSwagger2
 public class SpringBootSqlServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSqlServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSqlServerApplication.class, args);
+    }
 
 }
